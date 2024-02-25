ce22447fcafdcc9452acc936a3e7df42107378ce
dror <gabadr@post.bgu.ac.il>
2024-02-25 13:36:42 +0200

    update

--- config/redis.js
+++ config/redis.js
@@ -1,12 +1,19 @@
 const redis = require('async-redis');
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({
+    retry_strategy: function(options) {
+        if (options.error.code === "ECONNREFUSED" || options.attempt > 5 || options.total_retry_time > 1000 * 60 * 60) {
+            return new Error("Stop retry");
+        }
+        return Math.min(options.attempt * 100, 3000); // delay between retry attempts increases with each attemp
+    }
+});
 
 redisClient.on('ready', () => {
     console.log('Redis client connected');
 });
 
 redisClient.on('error', (error) => {
-    console.log(`Redis client failed to connect due to: ${error}`);
+    console.error(`Redis client failed to connect due to: ${error}`);
 });
 
 module.exports = { redisClient };
--- controllers/shortUrlController.js
+++ controllers/shortUrlController.js
@@ -16,7 +16,9 @@ async function handleShortUrlCreation(req, res) {
         const shortUrl = shortId.generate();
         const urls = getUrlCollection();
         await urls.insertOne({ "_id": shortUrl, "url": url, "createdAt": new Date() });
-        await redisClient.set(shortUrl, url, 'EX', REDIS_EXPIRY);
+        try {
+            await redisClient.set(shortUrl, url, 'EX', REDIS_EXPIRY);
+        } catch (error) {}
         res.json({ shortUrl: `http://localhost:3000/${shortUrl}` });
     } catch (error) {
         console.error(error);
@@ -27,14 +29,20 @@ async function handleShortUrlCreation(req, res) {
 async function handleUrlRedirection(req, res) {
     try {
         const { shortUrl } = req.params;
-        originUrl = await redisClient.get(shortUrl);
+        try {
+            originUrl = await redisClient.get(shortUrl);
+        } catch (error) {
+            originUrl = null;
+        }
         if (!originUrl) {
             const urls = getUrlCollection();
             const document = await urls.findOne({ "_id": shortUrl });
             if (document) {
                 console.log('Found in remote DB');
                 originUrl = document["url"];
-                await redisClient.set(shortUrl, originUrl, 'EX', REDIS_EXPIRY);
+                try {
+                    await redisClient.set(shortUrl, originUrl, 'EX', REDIS_EXPIRY);
+                } catch (error) {}
             } else {
                 return res.status(404).send('URL not found');
             }
